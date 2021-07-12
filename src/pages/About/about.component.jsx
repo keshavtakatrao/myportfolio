a0eb6681f4ccdc8b93ba0a75813d04ef04e752d6
keshavtakatrao <takatraokeshav@gmail.com>
2021-07-12 12:57:56 +0530

    resume url changed

--- src/pages/About/about.component.jsx
+++ src/pages/About/about.component.jsx
@@ -31,7 +31,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/136SeyJmayMsaLDB83ilwcpZ-KaOedT_n/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1kThtd45Mq1ew-HzEVuKQ3LfROvzF-lgg/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
